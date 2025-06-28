cedaae380a6207dc4c71268c554bcd18edcc581e
Yseek <yun5003naver.com>
2025-06-28 15:35:55 +0900

    🐛 tailwindcss 설정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, 
-    autoprefixer: {}, // 브라우저 호환성을 위한 접두사를 자동으로 붙여줍니다.
+    '@tailwindcss/postcss': {}, // ◀◀ 이 부분이 맞는지 확인
+    'autoprefixer': {},
   },
 };
 
