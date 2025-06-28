bd43fd6120b52514b90cd7f9db346b7277877558
Yseek <yun5003naver.com>
2025-06-28 15:19:06 +0900

    🐛 오류 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, 
     autoprefixer: {}, // 브라우저 호환성을 위한 접두사를 자동으로 붙여줍니다.
   },
 };
